9afb527318faa33e6d1283efdf717b9f4635e65b
Stephane Roy <stephroy411@hotmail.com>
2021-12-29 17:08:09 -0400

    Add SkillSection to projects and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import SkillSection from '../components/SkillSection';
+
 
 const About = () => {
   return (
   <>
     <AboutSection />
+    <SkillSection />
   </> 
   )
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import InfoSection from '../components/InfoSection';
+import SkillSection from '../components/SkillSection';
+
 
 const Projects = () => {
   return (
   <>
     <InfoSection/>
+    <SkillSection/>
   </> 
   )
 };
